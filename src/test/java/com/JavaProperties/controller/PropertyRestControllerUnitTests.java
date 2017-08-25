07e5864ce6132054ff2999e6b4d8cd322f5ce8d0
Rule1001 <hello@rule1001.com>
2017-08-25 14:02:15 +0100

    created test file

--- src/test/java/com/JavaProperties/controller/PropertyRestControllerUnitTests.java
+++ src/test/java/com/JavaProperties/controller/PropertyRestControllerUnitTests.java
@@ -1,4 +1,27 @@
 package com.JavaProperties.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.JavaProperties.controller.rest.PropertyRestController;
+import com.JavaProperties.model.PropertyModel;
+import com.JavaProperties.repository.PropertyRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class PropertyRestControllerUnitTests {
 }
