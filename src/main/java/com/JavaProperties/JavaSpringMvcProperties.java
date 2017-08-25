4249e56df17bbdd9868af321b67592337febf65a
Rule1001 <hello@rule1001.com>
2017-08-25 12:22:02 +0100

    added JavaSpringMVC config

--- src/main/java/com/JavaProperties/JavaSpringMvcProperties.java
+++ src/main/java/com/JavaProperties/JavaSpringMvcProperties.java
@@ -1,4 +1,14 @@
 package com.JavaProperties;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class JavaSpringMvcProperties {
+
+    public static void main(String...args) {
+
+        SpringApplication.run(JavaSpringMvcProperties.class, args);
+    }
 }
