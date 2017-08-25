cfed76cdda094f58a358cbb85cb32ead43e210d3
Rule1001 <hello@rule1001.com>
2017-08-25 10:43:02 +0100

    property model built

--- src/main/java/com/JavaProperties/model/PropertyModel.java
+++ src/main/java/com/JavaProperties/model/PropertyModel.java
@@ -1,4 +1,21 @@
 package com.JavaProperties.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+
+
+@Entity
 public class PropertyModel {
+
+    private Long propertyId = null;
+    private String propertyName = null;
+    private String propertyType = null;
+    private int bedrooms;
+    private String location = null;
+    private String PropertyDescription = null;
+    private boolean forSale = false;
+    private String imageUrl = null;
 }
